e3be7df7fb16345f60e0bff6cfc4534dbcae64e1
axellebersier <100302667+axellebersier@users.noreply.github.com>
2022-09-27 13:41:04 +0200

    modifs html

--- index.js
+++ index.js
@@ -1,33 +1,40 @@
-//définition des variables
+// modification de la carte sur web
 
-// Limiter la carte à la Suisse
-mymap.setMaxBounds([[45.5, 5.9], [48.0, 10.5]]);
-mymap.setMinZoom(7);
-mymap.setMaxZoom(12);
+//initialisation des variables 
 
-// Nous faisons une petite animation pour attirer l'oeil de l'utilisateur à la
-// boîte d'info les 3 premières fois. Cette variable compte combien de fois
-// l'animation a déjà eu lieu.
-var animation_count = 0;
 
-// Définir les différentes couches de base:
-var topoMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
-  attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
-  maxZoom: 16
+// coordonnées
+var mymap = new L.Map('map', {
+  center: [46.712004980312685, 8.366493210196497],
+  minZoom: 2,
+  maxZoom: 5,
+  zoom: 3
 });
+
+// fond de carte 
+var osmLayer = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+  attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+});
+
 var esriImagery = L.tileLayer(
   'http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
     attribution: '&copy; <a href="http://www.esri.com">Esri</a>, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-  }
-);
+  });
 
-// Ajouter la couche de base par défaut à la carte.
-topoMap.addTo(mymap);
+osmLayer.addTo(mymap);
 
 // Créer les boutons pour changer la couche de base
 var baseLayers = {
-  "Carte": topoMap,
+  "Carte": osmLayer,
   "Photos aériennes": esriImagery
 };
+
 var overlays = {};
 L.control.layers(baseLayers, overlays).addTo(mymap);
+
+// Afficher les coordonnées géographiques de l'emplacement de la souris
+// Les coordonnées sont affichées en bas à gauche, sous la carte
+mymap.on('mousemove', function(e){
+  var coord = e.latlng;
+  $('#coordonnees').html('Coordonnées: ' + coord.lat.toFixed(5) +' / '+ coord.lng.toFixed(5));
+});
